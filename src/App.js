6905c0819fc12680bdaf9af1ee857ed3296ce28d
Gerald-Star <ukwuomagerald@gmail.com>
2022-05-29 12:53:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+
+export default function App() {
+  const firstName = 'Joe'
+  const lastName = 'Chima'
+  const date = new Date()
+   
+  // OR more short code decalre date.getHours as const
+
+  const hours = date.getHours() % 15
+
+// return is what your component needs to show on the screen
+  
+  // anothher example with else if
+
+    const time = date.getHours()
+    let timeOfDay
+
+    if (time < 12) {
+    timeOfDay = 'morning'
+
+      // changing of here returns different time
+    } else if (time >= 10 && time < 12) {
+    timeOfDay = 'afternoon'
+    } else {
+    timeOfDay = 'night'
+    }
+
+   return ( 
+    <div>
+      {/* the hello still maintains the jsx mode.
+      Anything inside the curly bracket is considered as a javascript code */}
+      <h1>Hello {firstName} {lastName}</h1>
+      <h2> I have a date by {date.getHours() % 20} o'clock!</h2>
+
+      {/* short code by passing hours in js curly bracket */}
+
+       <p>It is currently about {hours} o'clock!</p>
+       
+    
+       <h3>it is {timeOfDay}</h3>
+       
     </div>
-  );
+  )
 }
 
-export default App;
